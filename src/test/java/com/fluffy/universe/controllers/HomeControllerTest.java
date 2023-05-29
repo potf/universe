db9cc33f3182424e71121363bdccc835009c0735
potf <romanmvp@gmail.com>
2023-05-29 13:05:55 +0300

    Publish Test Results

--- src/test/java/com/fluffy/universe/controllers/HomeControllerTest.java
+++ src/test/java/com/fluffy/universe/controllers/HomeControllerTest.java
@@ -10,11 +10,9 @@ import org.mockito.internal.verification.VerificationModeFactory;
 public class HomeControllerTest {
     @Test
     public void registerRoutestest() {
-        
         Javalin application=Mockito.mock(Javalin.class);
         HomeController hc=new HomeController(application);
         hc.registerRoutes(application);
         Mockito.inOrder(application).verify(application, VerificationModeFactory.calls(3)).get(Mockito.anyString(), Mockito.any(Handler.class), Mockito.any(Role.class));
-
     }
 }
